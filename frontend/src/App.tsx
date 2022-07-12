8bce3b398dc818bc504202899405e954d3ec3cb9
Gustavo Henrique Batista <gu.henrique.batista77@gmail.com>
2022-07-11 21:46:49 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import logo from './logo.svg'
-import './App.css'
+import NotificationButton from './components/NotificationButton'
 
 function App() {
-  return(
-    <h1>Olá mundo!</h1>
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+    </>
   )
- }
+}
 
 export default App
