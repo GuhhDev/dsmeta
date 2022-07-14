798d6e61c26e3fa016d81c92f997257ee935a10d
Gustavo Henrique Batista <gu.henrique.batista77@gmail.com>
2022-07-14 16:31:25 -0300

    tentando arrumar

--- backend/src/main/java/com/gustavoprojects/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/gustavoprojects/dsmeta/controllers/SaleController.java
@@ -1,8 +1,10 @@
 package com.gustavoprojects.dsmeta.controllers;
 
 import com.gustavoprojects.dsmeta.entities.Sale;
+import io.swagger.annotations.Api;
 import com.gustavoprojects.dsmeta.services.SaleService;
 import com.gustavoprojects.dsmeta.services.SmsService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
+@Api(value = "/sales", tags = "DSMeta API", description = "API Para Consultar Vendas e Enviar Notificacao via SMS com os Dados dos Melhores Vendedores")
 public class SaleController {
 
     @Autowired
@@ -21,6 +24,7 @@ public class SaleController {
     private SmsService smsService;
 
     @GetMapping
+    @ApiOperation(value = "Fetch all sales details", notes = "get all sales")
     public Page<Sale> findSales(
             @RequestParam(value="minDate", defaultValue = "") String minDate,
             @RequestParam(value="maxDate", defaultValue = "") String maxDate,
@@ -29,6 +33,7 @@ public class SaleController {
     }
 
     @GetMapping("/{id}/notification")
+    @ApiOperation(value = "Send a SMS notification by Id Sale", notes = "sends a notification SMS with the Best Seller Details")
     public void sendNotifySmsWithId(@PathVariable Long id){
         smsService.sendSmsWithId(id);
     }
