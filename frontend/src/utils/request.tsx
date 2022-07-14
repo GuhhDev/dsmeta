c9a1570f4fec5ba7b85f1b6742d7e47d249a573a
Gustavo Henrique Batista <gu.henrique.batista77@gmail.com>
2022-07-14 16:34:13 -0300

    erro front

--- frontend/src/utils/request.tsx
+++ frontend/src/utils/request.tsx
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://https://dsmeta-guhhdev.herokuapp.com:8080";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://https://dsmeta-guhhdev.herokuapp.com";
