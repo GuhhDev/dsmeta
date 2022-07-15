b4fac59dc0e26319fde6d5fa1269c60b4e792065
Gustavo Henrique Batista <gu.henrique.batista77@gmail.com>
2022-07-15 10:48:54 -0300

    commit

--- backend/src/main/java/com/gustavoprojects/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/gustavoprojects/dsmeta/services/SaleService.java
@@ -1,27 +1,30 @@
 package com.gustavoprojects.dsmeta.services;
 
-import com.gustavoprojects.dsmeta.entities.Sale;
-import com.gustavoprojects.dsmeta.repositories.SaleRepository;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
+import com.gustavoprojects.dsmeta.entities.Sale;
+import com.gustavoprojects.dsmeta.repositories.SaleRepository;
 
 @Service
 public class SaleService {
 
-    @Autowired
-    private SaleRepository saleRepository;
-
-    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
-        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
-        LocalDate min = maxDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
-        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
-        return saleRepository.findSales(min, max, pageable);
-    }
+	@Autowired
+	private SaleRepository repository;
+	
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable); 
+	}
 }
